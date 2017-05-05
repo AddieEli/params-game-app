86a2be76b15a859f38aaabd117ab46c306e7588a
AddieEli <acadavid71@gmail.com>
2017-05-04 21:25:32 -0500

    added new params

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -1,12 +1,20 @@
 class ParamsExamplesController < ApplicationController
 
-def query
-  @message = params["my_message"]
-  @message_2 = params["other_message"]
-end
+  def query
+   @message = params["my_message"]
+   @message_2 = params["other_message"]
+  end
 
-def url_segment
-  @message = params["this_is_a_key"]
-end
+  def url_segment
+    @message = params["this_is_a_key"]
+  end
+
+  def form_show
+
+  end 
+
+  def form_send
+    @message = params["form_message"]
+  end 
 
 end
